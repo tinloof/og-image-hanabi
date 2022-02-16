e4fd76ed84e8484dd94bbbe7be43cc89edd4df5e
Omar Benseddik <53658024+bnsddk@users.noreply.github.com>
2022-02-16 17:06:07 +0100

    Swap Vercel logo with Hanabi

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -52,8 +52,8 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 
 function getDefaultImages(images: string[], theme: Theme): string[] {
     const defaultImage = theme === 'light'
-        ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
-        : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
+        ? 'https://res.cloudinary.com/https-tinloof-com/image/upload/v1645027316/svg_hanabi_wo3sa9.svg'
+        : 'https://res.cloudinary.com/https-tinloof-com/image/upload/v1645027316/svg_hanabi_wo3sa9.svg';
 
     if (!images || !images[0]) {
         return [defaultImage];
--- web/index.ts
+++ web/index.ts
@@ -142,16 +142,12 @@ const markdownOptions: DropdownOption[] = [
 ];
 
 const imageLightOptions: DropdownOption[] = [
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'Hanabi', value: 'https://res.cloudinary.com/https-tinloof-com/image/upload/v1645027435/svg_black_hanabi_pxjzdt.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
 
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'Hanabi', value: 'https://res.cloudinary.com/https-tinloof-com/image/upload/v1645027316/svg_hanabi_wo3sa9.svg' },
 ];
 
 const widthOptions = [
@@ -205,7 +201,7 @@ const App = (_: any, state: AppState, setState: SetState) => {
         fontSize = '100px',
         theme = 'light',
         md = true,
-        text = '**Hello** World',
+        text = '**Hanabi** Agency',
         images=[imageLightOptions[0].value],
         widths=[],
         heights=[],
